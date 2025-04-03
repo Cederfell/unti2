5c1bdbc668ba7668a9edece97801171c645eed85
Ditt namn <cederfell@gmail.com>
2025-04-03 15:43:39 +0200

    hej

--- src/test/java/testUser.java
+++ src/test/java/testUser.java
@@ -1,6 +1,9 @@
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class testUser {
 
@@ -12,14 +15,14 @@ public class testUser {
     @Test
     public void testGetUserName() {
         User user = new User("Martin", "Hej12");
-        String expected = "Martin";
+        String expected = "Marti";
         String actual = user.getUserName();
         assertEquals(expected, actual);
     }
     @Test
     public void testGetPassword() {
         User user = new User("Martin", "Hej12");
-        String expected = "Hej123";
+        String expected = "Hej12";
         String actual = user.getPassword();
         assertEquals(expected, actual);
     }
